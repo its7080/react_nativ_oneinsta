478943e7d111ccc68680a0aa5af5892712d1f8fe
Anupam Manna <30969673+its7080@users.noreply.github.com>
2021-11-08 15:46:11 +0530

    Update Add.js

--- oneinsta/components/main/Add.js
+++ oneinsta/components/main/Add.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { Camera } from 'expo-camera';
+
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
@@ -19,47 +20,40 @@ export default function App() {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-  return (
-    <View style={styles.container}>
-      <Camera style={styles.camera} type={type}>
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={styles.text}> Flip </Text>
-          </TouchableOpacity>
-        </View>
-      </Camera>
+  return (    <View style={{ flex: 1 }}>
+    <View style={styles.cameraContainer}>
+      <Camera
+
+        style={styles.fixedRatio}
+        type={type}
+        ratio={'1:1'} />
     </View>
+
+    <Button
+      title="Flip Image"
+      onPress={() => {
+        setType(
+          type === Camera.Constants.Type.back
+            ? Camera.Constants.Type.front
+            : Camera.Constants.Type.back
+        );
+      }}>
+    </Button>
+    <Button title="Take Picture" onPress={() => takePicture()} />
+    <Button title="Pick Image From Gallery" onPress={() => pickImage()} />
+ 
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  cameraContainer: {
     flex: 1,
+    flexDirection: 'row'
   },
-  camera: {
+  fixedRatio: {
     flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    margin: 20,
-  },
-  button: {
-    flex: 0.1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-    color: 'white',
-  },
-});
+    aspectRatio: 1
+  }
+
+})
